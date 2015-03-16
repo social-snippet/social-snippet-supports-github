ba88d16133a0acf54b9fe1711335a511cbbe0540
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-16 17:05:58 +0900

    github_driver_spec: add tests for parse github info from url

--- spec/github_driver_spec.rb
+++ spec/github_driver_spec.rb
@@ -6,5 +6,18 @@ describe ::SocialSnippet::Repository::Drivers::GitHubDriver do
 
   include_context :TestDriver
 
+  context "url = git://github.com/social-snippet/example-repo.git", :current => true do
+    let(:repo_url) { "git://github.com/social-snippet/example-repo.git" }
+    let(:driver) { ::SocialSnippet::Repository::Drivers::GitHubDriver.new repo_url }
+    context "parse_github_owner" do
+      subject { driver.send :parse_github_owner }
+      it { should eq "social-snippet" }
+    end
+    context "parse_github_repo" do
+      subject { driver.send :parse_github_repo }
+      it { should eq "example-repo" }
+    end
+  end
+
 end
 
