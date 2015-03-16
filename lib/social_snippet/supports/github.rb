cc8e52965a4d2aea7a5178ea2fae567b49e5b418
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-16 16:02:09 +0900

    bundler: init gem

---
+++ lib/social_snippet/supports/github.rb
@@ -0,0 +1,9 @@
+require "social_snippet/supports/github/version"
+
+module SocialSnippet
+  module Supports
+    module Github
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/social_snippet/supports/github/version.rb
@@ -0,0 +1,7 @@
+module SocialSnippet
+  module Supports
+    module Github
+      VERSION = "0.1.0"
+    end
+  end
+end
