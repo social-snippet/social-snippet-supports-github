c611c643a5f5475748e538217136511f909cbcf2
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-16 16:43:16 +0900

    test: add tests for GitHubDriver

---
+++ spec/github_driver_spec.rb
@@ -0,0 +1,10 @@
+require "spec_helper"
+
+describe ::SocialSnippet::Repository::Drivers::GitHubDriver do
+
+  let(:driver_class) { ::SocialSnippet::Repository::Drivers::GitHubDriver }
+
+  include_context :TestDriver
+
+end
+
---
+++ spec/spec_helper.rb
@@ -0,0 +1,6 @@
+require "bundler/setup"
+
+require "social_snippet"
+require "social_snippet/supports/github"
+require "social_snippet/rspec/test_driver"
+
