45a9961c541c9821752b0923ed58eebb165a6799
Hiroyuki Sano <sh19910711@gmail.com>
2015-03-18 12:50:39 +0900

    test: add tests for target_url?() and target_path?()

--- spec/github_driver_spec.rb
+++ spec/github_driver_spec.rb
@@ -6,7 +6,7 @@ describe ::SocialSnippet::Repository::Drivers::GitHubDriver do
 
   include_context :TestDriver
 
-  context "url = git://github.com/social-snippet/example-repo.git", :current => true do
+  context "handle example-repo" do
     let(:repo_url) { "git://github.com/social-snippet/example-repo.git" }
     let(:driver) { ::SocialSnippet::Repository::Drivers::GitHubDriver.new repo_url }
     context "parse_github_owner" do
@@ -17,7 +17,33 @@ describe ::SocialSnippet::Repository::Drivers::GitHubDriver do
       subject { driver.send :parse_github_repo }
       it { should eq "example-repo" }
     end
-  end
+  end # url = example-repo
+
+  describe ".target_url?" do
+    context "target_url? git://github.com/user/repo" do
+      subject { driver_class.target_url? "git://github.com/user/repo" }
+      it { should be_truthy }
+    end
+    context "target_url? https://github.com/user/repo" do
+      subject { driver_class.target_url? "https://github.com/user/repo" }
+      it { should be_truthy }
+    end
+    context "target_url? http://github.com/user/repo" do
+      subject { driver_class.target_url? "http://github.com/user/repo" }
+      it { should be_truthy }
+    end
+    context "target_url? git://bitbucket.com/user/repo" do
+      subject { driver_class.target_url? "git://bitbucket.com/user/repo" }
+      it { should be_falsey }
+    end
+  end # .target_url?
+
+  describe ".target_path?" do
+    context "target_path? path/to/file" do
+      subject { driver_class.target_path? "path/to/repo" }
+      it { should be_falsey }
+    end
+  end # .target_path?
 
 end
 
